991bd8e69d7fb9e04f8292a5638c5b7ee94be3d8
Juan Manuel Rivera <jm.rivera@uniandes.edu.co>
2022-05-25 19:15:26 -0500

    Modifica el proyecto para permitir que haya interfaz gráfica

--- main.py
+++ main.py
@@ -1,50 +1,66 @@
 import streamlit as st
 from joblib import load
-from preproccess import TextPreprocessing
+import preproccess
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 # Configuración básica de la página
 st.set_page_config(
-  page_title="Laboratorio 2", layout="wide",
+  page_title="Analalizis de marcha", layout="wide",
 )
 
 # Clases proporcionadas por el negocio
 classes = {
-    1: "Neoplasms.",
-    2: "Digestive System Diseases.",
-    3: "Nervous System Diseases.",
-    4: "Cardiovascular Diseases.",
-    5: "General Pathological Conditions."
+    0: "no necesita cirugia",
+    1: "si necesita cirugía",
 }
+
+tipoCirugia={
+    1: "alargamiento del tendón de aquiles",
+    2: "alargamiento de la fascia",
+}
+
+tipoParalisis=["Ataxica",	"Diskinetica","Espastica",	"Hipotonica","Mixta"]
+categoriasSilver=["Negativo", "Leve", "Positivo", "No evaluable"]
 
 # Al tener que realizar la conexión una vez, utilizamos el decorador para no hacer varios llamados a la vez
 @st.experimental_singleton
 def upload():
     models = {
-    'grid_search_NB':{
-        "name":"Multinomial Bayes",
+    'knn_si_no':{
+        "name":"knn_si_no",
         "model": None
         },
-    'grid_search_pre':{
-        "name":"Ensemblers (Precision)",
+    'svm_cirugia':{
+        "name":"svm_cirugia",
         "model": None
-        },
-    'grid_search_knn':{
-        "name":"K-Nearest Neighbors",
+        },  
+    'scaler':{
+        "name":"scaler",
         "model": None
-        },     
+        }, 
     }
     for name, values in models.items():
-        values["model"] = load(f"classifiers/{name}.pkl")
+        values["model"] = load(f"classifiers/{name}.joblib")
 
     return models
 
 
 models = upload() # Cargamos nuestros modelos
-selection = st.multiselect("Models",models.keys(), list(models.keys())[0],format_func=lambda x: models.get(x)['name']) # Generamos una selección múltiple de nuestros modelos
+edad = st.number_input('Edad')
+tonoGemelos= st.number_input('Tono gastrocnemios')
+dorsiflexion= st.number_input('Dorsiflexión (en grados)')
+felxPlantar= st.number_input("Felxion Plantar Tobillo (en grados)")
+# silverskfold=st.number_input("Silfverskiold")
+silverskfold = st.selectbox(
+     'Silverskiöld',
+     categoriasSilver)
+tipoPar = st.selectbox(
+     'Indique el tipo de parálisis del paciente',
+     tipoParalisis)
 
-def predict(txt,selection):
+
+def predict(edad, tonoGemelos, dorsiflexion, felxPlantar, silverskfold, tipoPar):
     """
     Realiza la predicción del texto según los modelos seleccionados.
     Args:
@@ -56,24 +72,21 @@ def predict(txt,selection):
     fig = make_subplots()
     fig.update_layout(plot_bgcolor='rgba(0,0,0,0)')
 
-    for m in selection:
-        p = models[m]["model"].predict([txt])[0] # Obtenemos la clase del modelo según la predicción
-        proba = models[m]["model"].predict_proba([txt])[0] # Obtenemos las probabiidades de pertenencia de cada clase
-        st.markdown(f'For **{models[m]["name"]}**, the class predicted is *{classes[p]}*') # Mostramos el resultado apra cada uno de los modelos seleccionados
+    txt=preproccess.procesarInput(edad, tonoGemelos, dorsiflexion, felxPlantar, silverskfold, tipoPar, models['scaler']['model'])
+    print(txt)
 
-        # Añadimos cada predicción según su probabilidad.
-        fig.add_trace(
-            go.Bar(
-                x=list(classes.values()),
-                y=proba,
-                name=models[m]["name"]
-            ))
+    p = models['knn_si_no']["model"].predict(txt)[0] # Obtenemos la clase del modelo según la predicción
+    proba = models['knn_si_no']["model"].predict_proba(txt)[0] # Obtenemos las probabiidades de pertenencia de cada clase
+    labelClase=classes[p]
+    
+    #st.markdown('La prediccón es que el paciente '+labelClase + " con una probabilidad del "+str(proba[0]*100)+"%") # Mostramos el resultado apra cada uno de los modelos seleccionados
+    st.markdown('La prediccón es que el paciente '+labelClase) # Mostramos el resultado apra cada uno de los modelos seleccionados
 
-    fig.update_yaxes(range=[0,1])
-    fig.update_xaxes(title_text="Classes")
-    st.plotly_chart(fig,use_container_width=True)
+    if p ==1:
+        cirugia=models['svm_cirugia']['model'].predict(txt)[0]
+        labelCirugia=tipoCirugia[cirugia]
+        st.markdown('La cirugía predicha para este paciente es '+labelCirugia) # Mostramos el resultado apra cada uno de los modelos seleccionados
 
-txt = st.text_area("Medical Abstract", value="",height = 400) # Mostramos el área donde se copia el texto
-pred = st.button("Predict") # Botón para predecir
+pred = st.button("Predecir") # Botón para predecir
 if pred:
-    predict(txt, selection)
+    predict(edad, tonoGemelos, dorsiflexion, felxPlantar, silverskfold, tipoPar)
--- preproccess.py
+++ preproccess.py
@@ -1,107 +1,47 @@
-import re, unicodedata
 import contractions
-import inflect
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
+from sklearn.preprocessing import StandardScaler
 
 from sklearn.base import BaseEstimator,TransformerMixin
 import pandas as pd
 
+def procesarInput(edad, tono, dorsi, flexi, silvers, tipo, scaler):
+    atax,disk,espas,hipo,mix=0,0,0,0,0
 
-class TextPreprocessing(BaseEstimator,TransformerMixin):
-    """
-    Perzonalized transformer for text analysis. Proovided by sklearn.
-    """
-    def __init__(self,stopwords=stopwords.words('english')):
-        self.stopwords = stopwords
+    if tipo == "Ataxica":
+        atax=1
+    elif tipo == "Diskinetica":
+        disk=1
+    elif tipo == "Espastica":
+        espas=1
+    elif tipo == "Hipotonica":
+        hipo=1
+    else:
+        mix=1
 
-    def remove_non_ascii(self, words):
-        """Remove non-ASCII characters from list of tokenized words"""
-        new_words = []
-        for word in words:
-            new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-            new_words.append(new_word)
-        return new_words
+    if silvers == "Negativo":
+        silvers=0
+    elif silvers == "Leve":
+        silvers=1
+    elif silvers == "Positivo":
+        silvers=2
+    elif silvers == "No evaluable":
+        silvers=3
 
-    def to_lowercase(self, words):
-        """Convert all characters to lowercase from list of tokenized words"""
-        new_words = []
-        for word in words:
-            new_word = word.lower()
-            new_words.append(new_word)
-        return new_words
-
-    def remove_punctuation(self, words):
-        """Remove punctuation from list of tokenized words"""
-        new_words = []
-        for word in words:
-            new_word = re.sub(r'[^\w\s]', '', word)
-            if new_word != '':
-                new_words.append(new_word)
-        return new_words
-
-    def replace_numbers(self, words):
-        """Replace all interger occurrences in list of tokenized words with textual representation"""
-        p = inflect.engine()
-        new_words = []
-        for word in words:
-            if word.isdigit():
-                new_word = p.number_to_words(word)
-                new_words.append(new_word)
-            else:
-                new_words.append(word)
-        return new_words
-
-    def remove_stopwords(self, words):
-        """Remove stop words from list of tokenized words"""
-        new_words = []
-        for word in words:
-            if word not in self.stopwords:
-                new_words.append(word)
-        return new_words
-
-    def stem_words(self, words):
-        """Stem words in list of tokenized words"""
-        stemmer = LancasterStemmer()
-        stems = []
-        for word in words:
-            stem = stemmer.stem(word)
-            stems.append(stem)
-        return stems
-
-    def lemmatize_verbs(self, words):
-        """Lemmatize verbs in list of tokenized words"""
-        lemmatizer = WordNetLemmatizer()
-        lemmas = []
-        for word in words:
-            lemma = lemmatizer.lemmatize(word, pos='v')
-            lemmas.append(lemma)
-        return lemmas
-
-    def stem_and_lemmatize(self, words):
-        """Stematize and Lematize the words on corpus"""
-        words = self.stem_words(words)
-        words = self.lemmatize_verbs(words)
-        return words
-
-    def preproccesing(self, words):
-        """Unify all text cleaning proccess"""
-        words = self.to_lowercase(words)
-        words = self.replace_numbers(words)
-        words = self.remove_punctuation(words)
-        words = self.remove_non_ascii(words)
-        words = self.remove_stopwords(words)
-        return words
-
-    def fit(self,X,y=None):
-        return self
+    example={"Edad":[edad],"Tono gastrocnemios":[ tono ], "Dorsiflexion":[dorsi], "Felxion Plantar Tobillo":[flexi],
+    "Silfverskiold":[silvers],"Ataxica":[atax],"Diskinetica":[disk],"Espastica":[espas],"Hipotonica":[hipo],"Mixta":[mix]}
+    # Se convierte el diccionario a dataframe
+    entrada=pd.DataFrame.from_dict(example)
     
-    def transform(self,X,y=None):
-        new_X_train = pd.Series(X)
-        new_X_train = new_X_train.apply(contractions.fix)
-        new_X_train = new_X_train.apply(word_tokenize)
-        new_X_train = new_X_train.apply(lambda x: self.preproccesing(x))
-        new_X_train = new_X_train.apply(lambda x: self.stem_and_lemmatize(x))
-        new_X_train = new_X_train.apply(lambda x: ' '.join(map(str, x)))
-        return new_X_train
+    columns = ['Edad', 'Tono gastrocnemios', 'Dorsiflexion', 'Felxion Plantar Tobillo','Silfverskiold']
+    data_norm=entrada[columns]
+    print(data_norm)
+    data_norm = scaler.transform(data_norm)
+    print(data_norm)
+    data_norm = pd.DataFrame(data_norm,columns=columns)
+    data_norm[['Ataxica', 'Diskinetica','Espastica', 'Hipotonica', 'Mixta']]=entrada[['Ataxica', 'Diskinetica',
+       'Espastica', 'Hipotonica', 'Mixta']]
+
+    return data_norm
